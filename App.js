97b9f0a93c0703960602b6f3437261f7248822f4
n-varshni <153799618+n-varshni@users.noreply.github.com>
2023-12-14 12:06:23 +0530

    updated

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Login from './Components/login';
+import Signup from './Components/signup';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Signup/>
+    </div>
+  );
+}
+
+export default App;
